045d5d7d182170542a197e8d6cb14005fef6dbfc
Antonino DAnna <antonino.danna@estudiante.uam.es>
2026-05-18 11:11:17 +0200

    Added Abstract Boundary Condition Type

--- src/types.jl
+++ src/types.jl
@@ -1,10 +1,15 @@
+abstract type BC end
+
 """
         OBC
 empty structure used to exploit julia automatic dispacth. It selects the function to use when Open Boundary Conditions applies
 """
-struct OBC end
+struct OBC<:BC end
 """
         PBC
 empty structure used to exploit julia automatic dispacth. It selects the function to use when Periodic Boundary Conditions applies
 """
-struct PBC end
+struct PBC<:BC end
+
+Base.length(::BC) = 1
+Base.iterate(a::BC,state=1) = state>1 ? nothing : (a,state+1)
