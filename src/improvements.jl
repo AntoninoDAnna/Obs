f9992137cbfeef9bfe63805e5a6c7cdb8496c41f
Antonino D'Annaç <anda982008@gmail.com>
2026-06-26 18:58:59 +0200

    bugfix: Now the improvement function output vector of the same size of the input

--- src/improvements.jl
+++ src/improvements.jl
@@ -24,6 +24,12 @@ equations
 
 See also [`sym_der`](@ref), [`Boundary`](@ref)
 """
+function v_imp(vivi,vit0i,bnd::OBC; cv)
+    T = eltype(vivi)
+    der_vt = [zero(T);sym_der(vit0i[2:end-1],bnd);zero(T)]
+    return vivi.-2cv*der_vt
+end
+
 function v_imp(vivi,vit0i,bnd::B where {B<:BC}; cv)
     der_vt = sym_der(vit0i,bnd)
     return vivi.-2cv*der_vt
@@ -57,10 +63,10 @@ function v_imp(vivi::T, vit0i::T, v1t12::T,
 
     isnothing(theta1) && (theta1 = theta(vv)[1])
     isnothing(theta2) && (theta2 = theta(vv)[2])
-    imp = v_imp(vivi.obs[2:end-1], vit0i.obs[2:end-1],
-                v1t12.obs[2:end-1],v1t13.obs[2:end-1],
-                v2t12.obs[2:end-1],v2t23.obs[2:end-1],
-                v3t13.obs[2:end-1],v3t23.obs[2:end-1],bnd,
+    imp = v_imp(vivi.obs, vit0i.obs,
+                v1t12.obs,v1t13.obs,
+                v2t12.obs,v2t23.obs,
+                v3t13.obs,v3t23.obs,bnd,
                 cv=cv,L=L,theta1=theta1,theta2=theta2)
     return ObsIO.__update__(vv,obs=imp)
 end
@@ -95,14 +101,20 @@ Improve the correlator G_{PA_0} according to the equation
 
 See also [`sym_der`](@ref), [`Boundary`](@ref)
 """
+function pa0_imp(pa0, pp,bnd::OBC; ca)
+    T = eltype(pa0)
+    der_p= [zero(T);sym_det(pp[2:end-1],bnd);zero(T)]
+    return pa0.-ca.*der_p
+end
+
 function pa0_imp(pa0, pp,bnd::B where {B<:BC}; ca)
     der_p=sym_der(pp,bnd)
     return pa0.-ca.*der_p
 end
 
+
 function pa0_imp(pa0::T, pp::T, bnd::OBC; ca)::T where T<:AbstractCorr
-    imp = pa0_imp(pa0.obs[2:end-1],pp.obs[2:end-1],bnd,ca=ca)
-    _zero = zero(eltype(imp))
+    imp = pa0_imp(pa0.obs,pp.obs,bnd,ca=ca)
     return ObsIO.__update__(pa0,obs=[_zero;imp;_zero])
 end
 
@@ -125,13 +137,19 @@ Improve the correlator G_{A_0 A_0} according to the equation
 
 See also [`sym_der`](@ref), [`Boundary`](@ref)
 """
+function a0a0_imp(a0a0, pa0, bnd::OBC; ca)
+    T = eltype(a0a0)
+    der_pa0 = [zero(T);sym_der(pa0[2:end-1],bnd); zero(T)]
+    return a0a0 .- (2*ca).*der_pa0
+end
+
 function a0a0_imp(a0a0, pa0, bnd::B where {B<:BC}; ca)
     der_pa0 = sym_der(pa0,bnd)
     return a0a0 .- (2*ca).*der_pa0
 end
 
 function a0a0_imp(a0a0::T, pa0::T,bnd::OBC; ca )::T where T<:AbstractCorr
-    imp = a0a0_imp(a0a0.obs[2:end-1],pa0.obs[2:end-1],bnd,ca=ca)
+    imp = a0a0_imp(a0a0.obs,pa0.obs,bnd,ca=ca)
     return ObsIO.__update__(a0a0,obs=imp)
 end
 
@@ -165,6 +183,12 @@ Compute the improved G_{PV} =  1/3 \\sum_{k=1}^3G_{PV_i} correlator according to
 See also [`sym_der`](@ref), [`Boundary`](@ref)
             """
 
+function pv_imp(pvi, pt0i,bnd::OBC; cv,real::Bool)
+    T = eltype(pvi)
+    der_pt = [zero(T);sym_der(pt0i[2:end-1],bnd); zero(T)]
+    return pvi .- cv.*der_pt # we only have access to T_{0i}, but we want T_{i0}
+end
+
 function pv_imp(pvi, pt0i,bnd::B where {B<:BC};cv,real::Bool)
     der_pt = sym_der(pt0i,bnd)
     return pvi .- cv.*der_pt # we only have access to T_{0i}, but we want T_{i0}
@@ -181,7 +205,7 @@ function pv_imp(pv,pt0i,pt12,pt13,pt23,bnd::B where {B<:BC}; cv,L::Int64=1,theta
 end
 
 function pv_imp(pvi::T, pt0i::T, bnd::OBC; cv,real::Bool)::T where T<:AbstractCorr
-    imp = pv_imp(pvi.obs[2:end-1],pt0i.obs[2:end-1],bnd,cv=cv,real=real)
+    imp = pv_imp(pvi.obs,pt0i.obs,bnd,cv=cv,real=real)
     return ObsIO.__update__(pvi,obs=imp)
 end
 
@@ -193,8 +217,8 @@ end
 function pv_imp(pv::T, pt0i::T, pt12::T, pt13::T, pt23::T, bnd::OBC; cv, L=1, theta1=nothing,theta2=nothing,real::Bool)::T where T<:AbstractCorr
     isnothing(theta1) && (theta1 = theta(pv)[1])
     isnothing(theta2) && (theta2 = theta(pv)[2])
-    imp =  pv_imp(pv.obs[2:end-1], pt0i.obs[2:end-1], pt12.obs[2:end-1],
-                  pt13.obs[2:end-1], pt23.obs[2:end-1],bnd,
+    imp =  pv_imp(pv.obs, pt0i.obs, pt12.obs,
+                  pt13.obs, pt23.obs,bnd,
                   cv=cv,L=L, theta1=theta1, theta2=theta2,real=real)
     return ObsIO.__update__(pv,obs=imp)
 end
@@ -240,10 +264,10 @@ function pv0_imp(pv0, pt01, pt02, pt03; theta1,theta2, cv,L::Int64=1,real::Bool)
 end
 
 function pv0_imp(pv0::T, pt01::T, pt02::T, pt03::T, ::OBC;cv,L=1,theta1 = nothing, theta2 = nothing,real::Bool)::T where T<:AbstractCorr
-    isnothing(theta1) && (theta1 = theta(pv)[1])
-    isnothing(theta2) && (theta2 = theta(pv)[2])
-    imp =  pv0_imp(pv0.obs[2:end-1], pt01.obs[2:end-1], pt02.obs[2:end-1],
-                   pt03.obs[2:end-1], cv=cv, L=L,theta1=theta1, theta2=theta2,real = real)
+    isnothing(theta1) && (theta1 = theta(pv0)[1])
+    isnothing(theta2) && (theta2 = theta(pv0)[2])
+    imp =  pv0_imp(pv0.obs, pt01.obs, pt02.obs,
+                   pt03.obs, cv=cv, L=L,theta1=theta1, theta2=theta2,real = real)
     return ObsIO.__update__(pv0,obs=imp)
 end
 
